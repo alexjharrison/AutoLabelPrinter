1b42b44cb7b6903881a520bce6a865456318735e
alexmon32 <alexmon32@live.com>
2017-08-21 14:20:14 -0400

    #2

--- AutoLabelPrinter/Form1.Designer.cs
+++ AutoLabelPrinter/Form1.Designer.cs
@@ -28,12 +28,152 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.onOffButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioGlobal = new System.Windows.Forms.RadioButton();
+            this.radioMicroHite = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(105, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Auto Label Printer";
+            // 
+            // onOffButton
+            // 
+            this.onOffButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.onOffButton.Location = new System.Drawing.Point(237, 152);
+            this.onOffButton.Name = "onOffButton";
+            this.onOffButton.Size = new System.Drawing.Size(154, 115);
+            this.onOffButton.TabIndex = 1;
+            this.onOffButton.Text = "Press to Start";
+            this.onOffButton.UseVisualStyleBackColor = true;
+            this.onOffButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(55, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Select CMM";
+            // 
+            // radioGlobal
+            // 
+            this.radioGlobal.AutoSize = true;
+            this.radioGlobal.Location = new System.Drawing.Point(59, 207);
+            this.radioGlobal.Name = "radioGlobal";
+            this.radioGlobal.Size = new System.Drawing.Size(83, 17);
+            this.radioGlobal.TabIndex = 3;
+            this.radioGlobal.TabStop = true;
+            this.radioGlobal.Text = "Global CMM";
+            this.radioGlobal.UseVisualStyleBackColor = true;
+            // 
+            // radioMicroHite
+            // 
+            this.radioMicroHite.AutoSize = true;
+            this.radioMicroHite.Location = new System.Drawing.Point(59, 231);
+            this.radioMicroHite.Name = "radioMicroHite";
+            this.radioMicroHite.Size = new System.Drawing.Size(73, 17);
+            this.radioMicroHite.TabIndex = 4;
+            this.radioMicroHite.TabStop = true;
+            this.radioMicroHite.Text = "Micro-Hite";
+            this.radioMicroHite.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(54, 317);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Status:";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 16);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(94, 97);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(366, 16);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(94, 97);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 8;
+            this.pictureBox2.TabStop = false;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.BackColor = System.Drawing.SystemColors.Control;
+            this.statusLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.statusLabel.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.Location = new System.Drawing.Point(139, 317);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(41, 25);
+            this.statusLabel.TabIndex = 9;
+            this.statusLabel.Text = "Off";
+            this.statusLabel.DoubleClick += new System.EventHandler(this.label4_DoubleClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(465, 425);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radioMicroHite);
+            this.Controls.Add(this.radioGlobal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.onOffButton);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Auto Label Printer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button onOffButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioGlobal;
+        private System.Windows.Forms.RadioButton radioMicroHite;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label statusLabel;
     }
 }
 
--- AutoLabelPrinter/Form1.cs
+++ AutoLabelPrinter/Form1.cs
@@ -16,5 +16,46 @@ namespace AutoLabelPrinter
         {
             InitializeComponent();
         }
+
+        private void label4_DoubleClick(object sender, EventArgs e)
+        {
+            Form squak = new Form2();
+            squak.Show();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ((radioGlobal.Checked==false) && (radioMicroHite.Checked==false))
+            {
+                MessageBox.Show("Please Choose CMM to Monitor","Error",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (statusLabel.Text.Equals("Off")==true)
+                turnOn();
+            else turnOff();
+        }
+
+        private void turnOn()
+        {
+            statusLabel.Text = "Monitoring...";
+            statusLabel.BackColor = System.Drawing.Color.LightGreen;
+            onOffButton.Text = "Press to Stop";
+            radioGlobal.AutoCheck = false;
+            radioMicroHite.AutoCheck = false;
+        }
+
+        private void turnOff()
+        {
+            statusLabel.Text = "Off";
+            statusLabel.BackColor = SystemColors.Control;
+            onOffButton.Text = "Press to Start";
+            radioGlobal.AutoCheck = true;
+            radioMicroHite.AutoCheck = true;
+        }
+
+    
+        
     }
 }
